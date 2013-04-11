bf1c29b7e2bdc1e60ee3ce644cf00d735782d827
Aleksey Drobnych <adrobnych@gmail.com>
2013-04-11 22:50:39 +0300

    added ConsoleRunner withmain

---
+++ src/com/tdl/console/ConsoleRunner.java
@@ -0,0 +1,12 @@
+package com.tdl.console;
+
+import com.tdl.core.Game;
+
+public class ConsoleRunner {
+
+	public static void main(String[] args) {
+		Game game = new Game();
+		game.start();
+	}
+
+}
