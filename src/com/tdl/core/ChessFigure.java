9738a11d0272dd9eca9b6535db8cd01e2b7ffbba
Aleksey Drobnych <adrobnych@gmail.com>
2013-04-13 18:42:20 +0300

    created impl for ChessFigure and FiguresHolder

--- src/com/tdl/core/ChessFigure.java
+++ src/com/tdl/core/ChessFigure.java
@@ -1,15 +1,23 @@
 package com.tdl.core;
 
 public class ChessFigure {
+	private FigureType type;
+	private FigureColor color;
+	
+	public ChessFigure(FigureType type, FigureColor color){
+		this.type = type;
+		this.color = color;
+	}
+	
 	public void moveToNewPosition(String boardCell){
 		
 	}
 	
 	public FigureType getFigureSymbol(){
-		return null;
+		return type;
 	}
 	
 	public FigureColor getFigureColor(){
-		return null;
+		return color;
 	}
 }
--- src/com/tdl/core/FiguresHolder.java
+++ src/com/tdl/core/FiguresHolder.java
@@ -1,11 +1,19 @@
 package com.tdl.core;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class FiguresHolder {
+	
+	private Map<String, ChessFigure> figures = new HashMap<>();
+	
 	public ChessFigure findFigureAtFirstPosition(String boardCell){
-		return new ChessFigure();
+		return figures.get(boardCell);
 	}
 	
 	public static FiguresHolder standardOrderFactory(){
-		return new FiguresHolder();
+		FiguresHolder holder = new FiguresHolder();
+		holder.figures.put("a8", new ChessFigure(FigureType.ROOK, FigureColor.BLACK));
+		return holder;
 	}
 }
