c7fe90366a863fba8d445154e75b481523f5ddc4
Aleksey Drobnych <adrobnych@gmail.com>
2013-04-13 14:34:57 +0300

    refined spec for Game

--- src/com/tdl/tests/unit/GameSpec.java
+++ src/com/tdl/tests/unit/GameSpec.java
@@ -19,9 +19,9 @@ public class GameSpec {
 	
 	//interfaces - dependencies
 	
-	//We have rework CIN from global static scope to object
-	CIN cin = new CIN();
-	Game game = new Game(cin);
+	//cin is unstable dependency to console ui. Lets depend on abstraction 
+	UIManager ui = new UIConsoleManager();
+	Game game = new Game(ui);
 
 	//naturally Mockery works with interfaces, so need additional efforts to work with 
 	//concrete files. Actually I don't see problem with one concrete instance of boardUpdater. We wont have several ones.
@@ -33,15 +33,36 @@ public class GameSpec {
 	
 	@Test
 	public void afterStartItShouldWaitForHumanMovementAndThenUpdateBoard() {
-
+		//setup
 		game.start();
 		
+		//protocols
 		context.checking(new Expectations() {{
-			oneOf(boardUpdater).updateAfterUserMovement("d2-d3");
+			oneOf(figures).findFigureAtFirstPosition("d2");   
+		}});
+		
+		context.checking(new Expectations() {{
+			oneOf(figure).moveToNewPosition("d3");   
+		}});
+		
+		context.checking(new Expectations() {{
+			//lets spec this more simple
+			oneOf(boardUpdater).update();
 		}});
 		    
-		cin.mimicUserInput("d2-d3");
-		
+		//actionUnderTest
+		ui.mimicUserInput("d2-d3");
+				
+		//final state of world
+		assertEquals(null, figures.findFigureAtFirstPosition("d2")); //not for further refactoring - use exception or NullObject instead
+		assertEquals(ChessFigure, figures.findFigureAtFirstPosition("d3").class);
+		assertEqual(PAWN, figures.findFigureAtFirstPosition("d3").getFigureSymbol);
+		assertEqual(WHITE, figures.findFigureAtFirstPosition("d3").getFigureColor);
+
+		//note how quickly we can change spec of code - much faster then code itself.
+	    //This is one of parts of TD "miricle". 
+	
+	    //and there is no scare to fail. because we have tests as a safety belt
 	}
 
 }
