d5fd363a6344f4f91c1b9b1da515047fa8243d4e
Aleksey Drobnych <adrobnych@gmail.com>
2013-04-13 19:19:09 +0300

    more tests for FiguresHolder

--- src/com/tdl/core/FiguresHolder.java
+++ src/com/tdl/core/FiguresHolder.java
@@ -1,5 +1,7 @@
 package com.tdl.core;
 
+import static org.junit.Assert.assertEquals;
+
 import java.util.HashMap;
 import java.util.Map;
 
@@ -14,6 +16,29 @@ public class FiguresHolder {
 	public static FiguresHolder standardOrderFactory(){
 		FiguresHolder holder = new FiguresHolder();
 		holder.figures.put("a8", new ChessFigure(FigureType.ROOK, FigureColor.BLACK));
+		holder.figures.put("b8", new ChessFigure(FigureType.KNIGHT, FigureColor.BLACK));
+		holder.figures.put("c8", new ChessFigure(FigureType.BISHOP, FigureColor.BLACK));
+		holder.figures.put("d8", new ChessFigure(FigureType.QUEEN, FigureColor.BLACK));
+		holder.figures.put("e8", new ChessFigure(FigureType.KING, FigureColor.BLACK));
+		holder.figures.put("f8", new ChessFigure(FigureType.BISHOP, FigureColor.BLACK));
+		holder.figures.put("g8", new ChessFigure(FigureType.KNIGHT, FigureColor.BLACK));
+		holder.figures.put("h8", new ChessFigure(FigureType.ROOK, FigureColor.BLACK));
+		
+		for(char col='a'; col<='h'; col++)
+			holder.figures.put(col + "7", new ChessFigure(FigureType.PAWN, FigureColor.BLACK));
+		
+		for(char col='a'; col<='h'; col++)
+			holder.figures.put(col + "2", new ChessFigure(FigureType.PAWN, FigureColor.WHITE));
+		
+		holder.figures.put("a1", new ChessFigure(FigureType.ROOK, FigureColor.WHITE));
+		holder.figures.put("b1", new ChessFigure(FigureType.KNIGHT, FigureColor.WHITE));
+		holder.figures.put("c1", new ChessFigure(FigureType.BISHOP, FigureColor.WHITE));
+		holder.figures.put("d1", new ChessFigure(FigureType.QUEEN, FigureColor.WHITE));
+		holder.figures.put("e1", new ChessFigure(FigureType.KING, FigureColor.WHITE));
+		holder.figures.put("f1", new ChessFigure(FigureType.BISHOP, FigureColor.WHITE));
+		holder.figures.put("g1", new ChessFigure(FigureType.KNIGHT, FigureColor.WHITE));
+		holder.figures.put("h1", new ChessFigure(FigureType.ROOK, FigureColor.WHITE));
+		
 		return holder;
 	}
 }
--- src/com/tdl/tests/unit/FiguresHolderSpec.java
+++ src/com/tdl/tests/unit/FiguresHolderSpec.java
@@ -1,10 +1,11 @@
 package com.tdl.tests.unit;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
 import com.tdl.core.ChessFigure;
+import com.tdl.core.FigureColor;
 import com.tdl.core.FigureType;
 import com.tdl.core.FiguresHolder;
 
@@ -16,6 +17,31 @@ public class FiguresHolderSpec {
 		FiguresHolder figures = FiguresHolder.standardOrderFactory();
 		ChessFigure foundFigure = figures.findFigureAtFirstPosition("a8");
 		assertEquals(FigureType.ROOK, foundFigure.getFigureSymbol());
+		assertEquals(FigureColor.BLACK, foundFigure.getFigureColor());
+	}
+	
+	@Test
+	public void itCanGenerateDefauldOrderOfFigures_checkForWhitePawnAtH2() {
+		FiguresHolder figures = FiguresHolder.standardOrderFactory();
+		ChessFigure foundFigure = figures.findFigureAtFirstPosition("h2");
+		assertEquals(FigureType.PAWN, foundFigure.getFigureSymbol());
+		assertEquals(FigureColor.WHITE, foundFigure.getFigureColor());
+	}
+	
+	@Test
+	public void itCanGenerateDefauldOrderOfFigures_checkForBlackPawnAtG7() {
+		FiguresHolder figures = FiguresHolder.standardOrderFactory();
+		ChessFigure foundFigure = figures.findFigureAtFirstPosition("g7");
+		assertEquals(FigureType.PAWN, foundFigure.getFigureSymbol());
+		assertEquals(FigureColor.BLACK, foundFigure.getFigureColor());
+	}
+	
+	@Test
+	public void itCanGenerateDefauldOrderOfFigures_checkForWhiteKingAtE1() {
+		FiguresHolder figures = FiguresHolder.standardOrderFactory();
+		ChessFigure foundFigure = figures.findFigureAtFirstPosition("e1");
+		assertEquals(FigureType.KING, foundFigure.getFigureSymbol());
+		assertEquals(FigureColor.WHITE, foundFigure.getFigureColor());
 	}
 
 }
